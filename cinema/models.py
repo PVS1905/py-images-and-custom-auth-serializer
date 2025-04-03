4d832eb99f49c2e3eb0ba04b038fef5a9bf03b19
PVS1905 <viktor66462@gmail.com>
2025-04-03 19:04:58 +0300

    Solution

--- cinema/models.py
+++ cinema/models.py
@@ -1,6 +1,12 @@
+import os
+import pathlib
+import uuid
+from uuid import uuid4
+
 from django.core.exceptions import ValidationError
 from django.db import models
 from django.conf import settings
+from django.utils.text import slugify
 
 
 class CinemaHall(models.Model):
@@ -35,12 +41,30 @@ class Actor(models.Model):
         return f"{self.first_name} {self.last_name}"
 
 
+def movie_image_path(instance: "Movie", filename) -> pathlib.Path:
+    filename = (
+        f"{slugify(instance.title)}-{uuid.uuid4()}"
+        + pathlib.Path(filename).suffix
+    )
+    return pathlib.Path("upload/movies/") / pathlib.Path(filename)
+
+
+#
+# def movie_image_path(instance, filename):
+#    _, extension = os.path.splitext(filename)
+#    return os.path.join(
+#        "uploads/movies/",
+#        f"{slugify(instance.title)}-{uuid.uuid4()}{extension}"
+#    )
+
+
 class Movie(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
     duration = models.IntegerField()
     genres = models.ManyToManyField(Genre)
     actors = models.ManyToManyField(Actor)
+    image = models.ImageField(null=True, upload_to=movie_image_path)
 
     class Meta:
         ordering = ["title"]
@@ -122,9 +146,8 @@ class Ticket(models.Model):
         )
 
     def __str__(self):
-        return (
-            f"{str(self.movie_session)} (row: {self.row}, seat: {self.seat})"
-        )
+        return (f"{str(self.movie_session)} "
+                f"(row: {self.row}, seat: {self.seat})")
 
     class Meta:
         unique_together = ("movie_session", "row", "seat")
